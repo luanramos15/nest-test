74365a0e0aaf4e12dcc75f4cd17891aee4111375
Luan Azevedo <luan_ramos15@hotmail.com>
2026-02-26 15:12:43 -0300

    DB Specific Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'mysql_db',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'test',
-      entities: [],
-      synchronize: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
